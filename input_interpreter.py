8409a5998bd79003d5d1b06d50861e52f5f708e8
Domagalsky <adam.domagalski1994@gmail.com>
2016-10-17 10:12:39 +0200

    Zaczynam ogarniać NLTK

---
+++ input_interpreter.py
@@ -0,0 +1,4 @@
+import nltk
+tekst = " Dzisiaj w Pozanniu jest okropna pogoda"
+tokenz =  nltk.word_tokenize(tekst)
+print(tokenz)
--- main.py
+++ main.py
@@ -7,7 +7,3 @@ for contributor in contributors:
 
 contributors.append('Tomasz')
 
-import nltk
-tekst = " Dzisiaj w Pozanniu jest okropna pogoda"
-tokenz =  nltk.word_tokenize(tekst)
-print(tokenz)
