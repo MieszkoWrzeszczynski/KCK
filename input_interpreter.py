6fd0ff59f8205dce1629560368fafe86546e2b1a
Domagalsky <adam.domagalski1994@gmail.com>
2016-10-17 10:28:48 +0200

    TODO https://www.jetbrains.com/help/pycharm/2016.1/using-todo.html

--- input_interpreter.py
+++ input_interpreter.py
@@ -1,4 +1,9 @@
+
+# Warning Ogarnąć jak to działa
+# TODO: test 123
 import nltk
 tekst = " Dzisiaj w Pozanniu jest okropna pogoda"
 tokenz =  nltk.word_tokenize(tekst)
 print(tokenz)
+
+#FIXME jak to naprawic?? - Adaś
