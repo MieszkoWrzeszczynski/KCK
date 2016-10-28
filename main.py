1165208985f824cc8ddae3100780fee24ba93f0b
MaciejWanat <macio-111@o2.pl>
2016-10-28 08:44:41 +0200

    Dodanie testowego kalkulatora i bibliotek

--- main.py
+++ main.py
@@ -1,3 +1,65 @@
 #!/usr/bin/python3
 
-print('Hello world')
+import ply.lex as lex
+import ply.yacc as yacc
+
+tokens = (
+    'NUMBER',
+    'PLUS',
+    'MINUS',
+    'TIMES',
+    'DIVIDE',
+    )
+
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_DIVIDE = r'/'
+
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+t_ignore = " \t"
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+lex.lex()
+
+precedence = (
+    ('left', 'PLUS', 'MINUS'),
+    ('left', 'TIMES', 'DIVIDE'),
+    )
+
+def p_statement_expr(p):
+    'statement : expression'
+    print(p[1])
+
+def p_expression_binop(p):
+    '''expression : expression PLUS expression
+                  | expression MINUS expression
+                  | expression TIMES expression
+                  | expression DIVIDE expression'''
+    if p[2] == '+'  : p[0] = p[1] + p[3]
+    elif p[2] == '-': p[0] = p[1] - p[3]
+    elif p[2] == '*': p[0] = p[1] * p[3]
+    elif p[2] == '/': p[0] = p[1] / p[3]
+
+def p_expression_number(p):
+    'expression : NUMBER'
+    p[0] = p[1]
+
+def p_error(p):
+    print("Syntax error at '%s'" % p.value)
+
+yacc.yacc()
+
+while True:
+    try:
+        s = input('> ')
+    except EOFError:
+        break
+    yacc.parse(s)
