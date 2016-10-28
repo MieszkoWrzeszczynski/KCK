f69090e8a53a3e61151334e2f263b0c1c21fc077
AdamDomagalsky <adam.domagalski1994@gmail.com>
2016-10-28 09:51:25 +0200

    Siemaneczko

--- input.py
+++ input.py
@@ -4,3 +4,6 @@ def normalize(input: str)->str:
     result = re.findall(r'\w+', input)
     result = list(map(lambda x: x.lower(), result))
     return result
+
+
+print (normalize("To jest przykładowy tekst"))
