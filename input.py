fff6e69c78655f366e2ca81fe38b106e93d2517f
MaciejWanat <macio-111@o2.pl>
2016-10-28 08:37:32 +0200

    Nowy branch

---
+++ input.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+import re
+
+def normalize(input: str)->str:
+    result = re.findall(r'\w+', input)
+    result = list(map(lambda x: x.lower(), result))
+    return result
--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-print("Hello, world!")
+#!/usr/bin/python3
 
-contributors = ['Mieszko','Adam','Maciej']
-
-for contributor in contributors:
-    print(contributor)
-
-contributors.append('Tomasz')
-
-import nltk
-tekst = " Dzisiaj w Pozanniu jest okropna pogoda"
-tokenz =  nltk.word_tokenize(tekst)
-print(tokenz)
+print('Hello world')
