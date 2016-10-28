a24404edca09ecbd860e62792fdc551c30c2e30f
MaciejWanat <macio-111@o2.pl>
2016-10-28 09:27:11 +0200

    Podsawy lexera

--- main.py
+++ main.py
@@ -2,24 +2,18 @@
 
 import ply.lex as lex
 import ply.yacc as yacc
+import replacePolish
 
 tokens = (
-    'NUMBER',
-    'PLUS',
-    'MINUS',
-    'TIMES',
-    'DIVIDE',
+    'CMD',
+    'DIRECTION',
+    'OTHER'
     )
 
-t_PLUS = r'\+'
-t_MINUS = r'-'
-t_TIMES = r'\*'
-t_DIVIDE = r'/'
+t_CMD = r'idź'
+t_DIRECTION = r'(lewo|prawo|góra|dół)'
 
-def t_NUMBER(t):
-    r'\d+'
-    t.value = int(t.value)
-    return t
+t_OTHER = r'\w+'
 
 t_ignore = " \t"
 
@@ -27,26 +21,15 @@ def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
-lex.lex()
-
-precedence = (
-    ('left', 'PLUS', 'MINUS'),
-    ('left', 'TIMES', 'DIVIDE'),
-    )
-
+lexer = lex.lex()
+'''
 def p_statement_expr(p):
     'statement : expression'
     print(p[1])
 
 def p_expression_binop(p):
-    '''expression : expression PLUS expression
-                  | expression MINUS expression
-                  | expression TIMES expression
-                  | expression DIVIDE expression'''
-    if p[2] == '+'  : p[0] = p[1] + p[3]
-    elif p[2] == '-': p[0] = p[1] - p[3]
-    elif p[2] == '*': p[0] = p[1] * p[3]
-    elif p[2] == '/': p[0] = p[1] / p[3]
+    'expression : expression DIRECTION '
+    if
 
 def p_expression_number(p):
     'expression : NUMBER'
@@ -56,10 +39,27 @@ def p_error(p):
     print("Syntax error at '%s'" % p.value)
 
 yacc.yacc()
-
+'''
+"""
 while True:
     try:
         s = input('> ')
     except EOFError:
         break
     yacc.parse(s)
+"""
+
+# Test it out
+data = '''
+idź lewo prawo prawostronny idź lewostronny tak
+'''
+
+# Give the lexer some input
+lexer.input(data)
+
+# Tokenize
+while True:
+    tok = lexer.token()
+    if not tok:
+        break      # No more input
+    print(tok)
