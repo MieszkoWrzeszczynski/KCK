5c4275c309042c7c3d947d332876da62675ed531
MaciejWanat <macio-111@o2.pl>
2016-10-28 10:06:06 +0200

    Gotowy podstawowy parser i lexer

--- main.py
+++ main.py
@@ -4,62 +4,38 @@ import ply.lex as lex
 import ply.yacc as yacc
 import replacePolish
 
+lexer
 tokens = (
     'CMD',
     'DIRECTION',
-    'OTHER'
+#    'OTHER'
     )
 
 t_CMD = r'idź'
 t_DIRECTION = r'(lewo|prawo|góra|dół)'
 
-t_OTHER = r'\w+'
-
+#t_OTHER = r'\w+'
 t_ignore = " \t"
 
 def t_error(t):
-    print("Illegal character '%s'" % t.value[0])
+#    print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
 lexer = lex.lex()
-'''
-def p_statement_expr(p):
-    'statement : expression'
-    print(p[1])
 
-def p_expression_binop(p):
-    'expression : expression DIRECTION '
-    if
-
-def p_expression_number(p):
-    'expression : NUMBER'
-    p[0] = p[1]
+#parser
+def p_expression_move(p):
+    'expression : CMD DIRECTION '
+    print('Ide')
 
 def p_error(p):
-    print("Syntax error at '%s'" % p.value)
+    print("Nie rozumiem!")
 
 yacc.yacc()
-'''
-"""
-while True:
-    try:
-        s = input('> ')
-    except EOFError:
-        break
+
+while True: # pętla główna
+    #try:
+    s  = input('> ')
+   # except EOFError:
+    #    break
     yacc.parse(s)
-"""
-
-# Test it out
-data = '''
-idź lewo prawo prawostronny idź lewostronny tak
-'''
-
-# Give the lexer some input
-lexer.input(data)
-
-# Tokenize
-while True:
-    tok = lexer.token()
-    if not tok:
-        break      # No more input
-    print(tok)
